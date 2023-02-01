13d2259b3d2a3174708e0008db83322664703f5c
Goldielockz30 <ahimbajohnson@hotmail.co.uk>
2023-02-01 14:46:19 +0000

    calculator

---
+++ calculator.py
@@ -0,0 +1,23 @@
+def addition(num1: int, num2: int) -> float:# type hint for return float
+    return num1 + num2
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+print(addition(num1, num2))
+
+def subtraction(num1: int, num2: int) -> float: # type hint for return float
+    return num1 - num2
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+print(subtraction(num1, num2))
+
+def multiplication(num1: int, num2: int) -> float: # type hint for return float
+    return num1 * num2
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+print(multiplication(num1, num2))
+
+def division(num1: int, num2: int) -> float: # type hint for return float
+    return num1 / num2
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+print(division(num1, num2))
