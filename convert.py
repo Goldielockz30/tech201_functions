b850efa01e43403af35f0dd8b2c4fded4995c032
Goldielockz30 <ahimbajohnson@hotmail.co.uk>
2023-02-01 15:56:40 +0000

    convert

--- convert.py
+++ convert.py
@@ -5,8 +5,7 @@ num2 = 100
 print("Your converted value is")
 print(convert(num1, num2))
 
-def convert(num1, num2: float) -> float:
-    return num1 / num2
+
 num1 = int(input("Enter the number of metres you would like to convert to feet: "))
 num2 = 3.280839895
 print("Your converted value is")
